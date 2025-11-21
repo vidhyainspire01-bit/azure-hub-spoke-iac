1fb1c04ce1f27beeb98f9eaebec8aa62e86cc0ef
Vidhya <vidhya.inspire01@gmail.com>
2025-11-21 20:07:35 +0530

    integrate agents in the current workflow

--- azure-hub-spoke/hub/hub.tf
+++ azure-hub-spoke/hub/hub.tf
@@ -20,6 +20,7 @@ resource "azurerm_virtual_network" "hub_vnet" {
   }
 }
 
+
 resource "azurerm_subnet" "AzureFirewallSubnet" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.hub.name
